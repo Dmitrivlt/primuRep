ab3ae1f2c55144ecb55c3f7d6b40d275dc4fb036
DimaPirvu <106433453+DimaPirvu@users.noreply.github.com>
2022-07-29 19:17:28 +0300

    treabata

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ClassCounter from './ClassCounter';
+import Counter from './Counter';
+
 
 function App() {
+  const [value, setValue] = useState('Текст в ИНПУТЕ')
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ClassCounter />
+    </div >
   );
 }
 
