9b7a4ff72cafbaa2474f048d908ac7205b7245c8
DimaPirvu <106433453+DimaPirvu@users.noreply.github.com>
2022-08-02 14:32:07 +0300

    progress

--- src/App.js
+++ src/App.js
@@ -1,52 +1,55 @@
-import React, { useState } from 'react';
+import { getByTitle } from '@testing-library/react';
+import React, { useRef, useState } from 'react';
+import PostItem from './components/PostItem';
+import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
 // import ClassCounter from './components/ClassCounter';
 // import Counter from './Counter';
 import './styles/App.css';
+import MyInput from './components/UI/input/MyInput'
 
 function App() {
-  const [value, setValue] = useState('Текст в ИНПУТЕ')
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'Javascript', body: 'Invatam' },
+    { id: 2, title: 'React', body: 'Acum la moment' },
+    { id: 3, title: 'Front-end', body: 'In process' },
+
+  ])
+  // // const [posts2, setPost2] = useState([
+  // //   { id: 1, title: 'Back-end', body: 'In asteptare' },
+  // //   { id: 2, title: 'Phyton', body: 'In asteptare' },
+  // //   { id: 3, title: 'Dream-work', body: 'Pizdets si tare vreu' }
+
+  // ])
+  const [post, setPost] = useState({ title: '', body: '' })
 
 
+  const addNewPost = (e) => {
+    e.preventDefault()
+    setPosts([...posts, { ...post, id: Date.now() }])
+    setPost({ title: '', body: '' })
+  }
 
   return (
     <div className="App">
-      <div className="post">
-        <div className="post_content">
-          <strong> 1. JavaScript</strong>
-          <div>
-            JavaScript - язык программирования!
-          </div>
-        </div>
-        <div className="post_btns">
-          <button>Удалить</button>
-        </div>
-
-      </div>
-      <div className="post">
-        <div className="post_content">
-          <strong> 1. JavaScript</strong>
-          <div>
-            JavaScript - язык программирования!
-          </div>
-        </div>
-        <div className="post_btns">
-          <button>Удалить</button>
-        </div>
-
-      </div>
-      <div className="post">
-        <div className="post_content">
-          <strong> 1. JavaScript</strong>
-          <div>
-            JavaScript - язык программирования!
-          </div>
-        </div>
-        <div className="post_btns">
-          <button>Удалить</button>
-        </div>
-
-      </div>
-
+      <form>
+        {/* Управляемый компонент */}
+        <MyInput
+          value={post.title}
+          onChange={e => setPost({ ...post, title: e.target.value })}
+          type="text"
+          placeholder="Название поста" />
+        {/* Не Управляемый компонент  */}
+        <MyInput
+          value={post.body}
+          onChange={e => setPost({ ...post, body: e.target.value })}
+          type="text"
+          placeholder="Описание поста"
+        />
+        <MyButton onChange={addNewPost} >  Создать пост  </MyButton>
+      </form>
+      <PostList posts={posts} title="Посты про Front-End" />
+      {/* <PostList posts={posts2} title="Посты про Back-End " /> */}
     </div >
   );
 }
